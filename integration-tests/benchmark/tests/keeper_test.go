5634217815e813c87b054c28673580da1c7efc94
anirudhwarrier <anirudhwarrier@users.noreply.github.com>
2023-01-27 23:37:40 +0530

    temp override for soak

--- integration-tests/benchmark/tests/keeper_test.go
+++ integration-tests/benchmark/tests/keeper_test.go
@@ -235,7 +235,7 @@ func TestKeeperBenchmarkOptimismGoerliRegistry_2_0(t *testing.T) {
 		int64(20),
 		predeployedConsumersEmpty,
 		upkeepResetterContractEmpty,
-		5 * time.Second,
+		4 * time.Second,
 	})
 }
 
